6a1faf9dffa6ea3b870eb95b4a99b4a7fb0c2200
Jainam Desai <jainamvipul.desai2019@vitstudent.ac.in>
2020-07-22 18:16:04 +0000

    INIT 2

---
+++ src/container/wishings/wishings.jsx
@@ -0,0 +1,11 @@
+import React, {Component } from "react"
+
+
+class Wishings extends Component {
+    render() {
+    return (<div className="wissings-div"><div></div></div>);
+    }
+}
+
+
+export default Wishings;
