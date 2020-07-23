56e7238c42ca497935ae15dd1427737e9d04c06e
th3c0d3br34ker <jainamvipul.desai2019@vitstudent.ac.in>
2020-07-23 11:18:40 +0530

    Updates

---
+++ precache-manifest.f7c822fc96f9d4a301a5d5876d218dfc.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "82ad6e560c2efad7c053e59a20d38adb",
+    "url": "/index.html"
+  },
+  {
+    "revision": "f7a94b2489adcf537221",
+    "url": "/static/css/main.129a7f32.chunk.css"
+  },
+  {
+    "revision": "24c171954a5a52b7af4b",
+    "url": "/static/js/2.0df7253e.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/static/js/2.0df7253e.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "f7a94b2489adcf537221",
+    "url": "/static/js/main.d6bed5df.chunk.js"
+  },
+  {
+    "revision": "56c354cb8cbe030e502a",
+    "url": "/static/js/runtime-main.ca860db0.js"
+  }
+]);
