a6b7ffa8f2a785f9b4feff5b2fd19d4dffd19a0d
th3c0d3br34ker <jainamvipul.desai2019@vitstudent.ac.in>
2020-07-24 18:40:07 +0530

    Updates

--- precache-manifest.4c8fbd8c4a522f1e8267ae4ad82d2db7.js
+++ precache-manifest.4c8fbd8c4a522f1e8267ae4ad82d2db7.js
@@ -1,11 +1,11 @@
 self.__precacheManifest = (self.__precacheManifest || []).concat([
   {
-    "revision": "a29e77d8f60ef34be878357146e9b1d7",
+    "revision": "628120b9ea1905590694f6454d7f5606",
     "url": "/hbd/index.html"
   },
   {
-    "revision": "ed5140023a62fe6fd4d8",
-    "url": "/hbd/static/css/main.9288ff39.chunk.css"
+    "revision": "f64553a08091915de110",
+    "url": "/hbd/static/css/main.e4692263.chunk.css"
   },
   {
     "revision": "24c171954a5a52b7af4b",
@@ -16,8 +16,8 @@ self.__precacheManifest = (self.__precacheManifest || []).concat([
     "url": "/hbd/static/js/2.0df7253e.chunk.js.LICENSE.txt"
   },
   {
-    "revision": "ed5140023a62fe6fd4d8",
-    "url": "/hbd/static/js/main.da4ca9d6.chunk.js"
+    "revision": "f64553a08091915de110",
+    "url": "/hbd/static/js/main.b0737a40.chunk.js"
   },
   {
     "revision": "7c85e47340376b702b0f",
