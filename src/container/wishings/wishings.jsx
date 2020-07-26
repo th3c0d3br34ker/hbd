aced53159eb23d72a4f72e1af70f538daa8a1f39
th3c0d3br34ker <jainamvipul.desai2019@vitstudent.ac.in>
2020-07-26 22:59:01 +0530

    Improve performance #3

--- src/container/wishings/wishings.jsx
+++ src/container/wishings/wishings.jsx
@@ -1,26 +1,22 @@
-import React, { Component } from "react";
+import React from "react";
 
 import Transition from "../../elements/Transition/transition.jsx";
 import { Messages } from "../../messages.js";
 
-class Wishings extends Component {
-  render() {
-    return (
-      <div className="wishings-div">
-        {Messages.map((message) => {
-          return (
-            <Transition
-              key={message.id}
-              id={message.id}
-              start={message.start}
-              message={message.message}
-              delay={message.delay}
-            />
-          );
-        })}
-      </div>
-    );
-  }
+export default function Wishings() {
+  return (
+    <div className="wishings-div">
+      {Messages.map((message) => {
+        return (
+          <Transition
+            key={message.id}
+            id={message.id}
+            start={message.start}
+            message={message.message}
+            delay={message.delay}
+          />
+        );
+      })}
+    </div>
+  );
 }
-
-export default Wishings;
--- src/elements/TypingDots/typingdots.jsx
+++ src/elements/TypingDots/typingdots.jsx
@@ -1,17 +1,13 @@
-import React, { Component } from "react";
+import React from "react";
 
-class TypingDots extends Component {
-  render() {
-    return (
-      <div className="typing-dots-box">
-        <div className="typingdots">
-          <span id="1"></span>
-          <span id="2"></span>
-          <span id="3"></span>
-        </div>
+export default function TypingDot() {
+  return (
+    <div className="typing-dots-box">
+      <div className="typingdots">
+        <span id="1"></span>
+        <span id="2"></span>
+        <span id="3"></span>
       </div>
-    );
-  }
+    </div>
+  );
 }
-
-export default TypingDots;
